6f3fd57ee550e0b345b9cb479e9b66714b3a3d42
Yaroslav <yaroslav.bemov@gmail.com>
2021-05-26 23:21:36 +0300

    add firestore party

--- src/components/main/Party.js
+++ src/components/main/Party.js
@@ -1,14 +1,46 @@
-import React, {memo} from 'react'
+import React, {memo, useEffect, useState} from 'react'
 import Meets from './Meets'
 import Meet from './Meet'
+import {db} from '../../adapters/firebase'
+import {useAuth} from '../../contexts/AuthContext'
 
 const Party = memo(() => {
     console.log('render party')
+    const [party, setParty] = useState([])
+    const {currentUser} = useAuth()
+    const id = currentUser.uid
+
+    useEffect(() => {
+        const unsubscribeParty = db
+            .collection('party')
+            .where('members', 'array-contains', id)
+            .get()
+            .then(docs => {
+                const data = []
+                docs.forEach(doc => {
+                    data.push({
+                        id: doc.id,
+                        ...doc.data()
+                    })
+                })
+                setParty(data)
+            })
+
+        return () => {
+            // unsubscribeParty()
+        }
+    }, [])
+
     return (
         <>
-        <section className='party'>
-            <h1>Party</h1>
-        </section>
+            <section className='party'>
+                <h1>Party</h1>
+                {party && party.map(item => (
+                    <div>
+                        {item.id}
+                    </div>
+                ))}
+            </section>
         </>
     )
 })
