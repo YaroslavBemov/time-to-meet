82b33f7d0a662fb739556ab55906f9fd38c8e5bc
Yaroslav <yaroslav.bemov@gmail.com>
2021-06-01 08:49:41 +0300

    meets with firebase

--- src/components/MeetsPage.js
+++ src/components/MeetsPage.js
@@ -1,23 +1,37 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {Link, Switch, Route, useRouteMatch} from 'react-router-dom'
 import MeetPage from './MeetPage'
+import {db} from '../adapters/firebase'
 
 const MeetsPage = () => {
+    const [meets, setMeets] = useState([])
     const match = useRouteMatch()
+    const party = match.params.party
+
+    useEffect(() => {
+        const unsubMeets = db
+            .collection('meets')
+            .where('party', '==', party)
+            .onSnapshot(snapshot => {
+                const list = snapshot.docs.map(doc => ({
+                    id: doc.id,
+                    ...doc.data()
+                }))
+                setMeets(list)
+            })
+    }, [party])
 
     return (
         <div>
             <h1>MEETS PAGE</h1>
             <ul>
-                <li>
-                    <Link to={`${match.url}/meet1`}>Meet 1</Link>
-                </li>
-                <li>
-                    <Link to={`${match.url}/meet2`}>Meet 2</Link>
-                </li>
-                <li>
-                    <Link to={`${match.url}/meet3`}>Meet 3</Link>
-                </li>
+                {!meets
+                    ? null
+                    : meets.map(item => (
+                        <li key={item.id}>
+                            <Link to={`${match.url}/${item.id}`}>{item.id}</Link>
+                        </li>
+                    ))}
             </ul>
 
             <hr/>
